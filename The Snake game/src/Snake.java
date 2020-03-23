3f5c0f462471f04e9b8febb3807b3b9a84e70691
DahimiSoufiane <DAHIMI Soufiane>
2020-03-23 15:45:09 +0100

    now we 've just impelement the snake class (with its fields and methods ) , we've done the same thing with the snakesection class

--- The Snake game/src/Snake.java
+++ The Snake game/src/Snake.java
@@ -1,11 +1,40 @@
 /**
  * 
  */
-
+import java.util.*;
 /**
  * @author pc
  *
  */
 public class Snake {
-
+	 private boolean isAlive ;
+	 private List<SnakeSection> sections ;
+	    private SnakeDirection direction ;
+	    public Snake(int x , int y){
+	        isAlive = true;
+	        SnakeSection head = new SnakeSection(x,y);
+	        sections = new ArrayList<SnakeSection>();
+	        sections.add(head);
+	    }
+	    public int getX(){
+	        return sections.get(0).getX();
+	    }
+	    public int getY(){
+	        return sections.get(0).getY();
+	    }
+	    public boolean isAlive(){
+	        return isAlive ;
+	    }
+	    public SnakeDirection getDirection(){
+	        return direction;
+	    }
+	    public void setDirection(SnakeDirection direction){
+	        this.direction = direction ;
+	    }
+	    public List<SnakeSection> getSections(){
+	        return sections;
+	    }
+	    public void move(){
+	        
+	    }
 }
