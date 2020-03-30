cb6aedc8fdd01edabd4a19c7c32ccdd44be8971d
DahimiSoufiane <DAHIMI Soufiane>
2020-03-30 11:59:44 +0100

    first we create the basic components of the game : the room(arkanoid), bricks, paddle,ball

---
+++ The Arkanoid game/src/Game_Package/Arkanoid.java
@@ -0,0 +1,32 @@
+package Game_Package;
+
+
+public class Arkanoid {
+	private int width ;
+	private int height;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	public Arkanoid(int width , int height){
+	    this.width = width;
+	    this.height = height;
+	}
+	public int getWidth() {
+		return width;
+	}
+
+	public void setWidth(int width) {
+		this.width = width;
+	}
+
+	public int getHeight() {
+		return height;
+	}
+
+	public void setHeight(int height) {
+		this.height = height;
+	}
+
+}
