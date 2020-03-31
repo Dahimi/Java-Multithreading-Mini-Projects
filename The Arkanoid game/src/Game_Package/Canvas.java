8add4a2238be6e32d49c8991c41f4c99c74fa190
DahimiSoufiane <DAHIMI Soufiane>
2020-03-31 16:21:29 +0100

    adding the painting functionalities to the canvas class)

--- The Arkanoid game/src/Game_Package/Canvas.java
+++ The Arkanoid game/src/Game_Package/Canvas.java
@@ -28,5 +28,22 @@ public class Canvas {
 	public void setMatrix(char[][] matrix) {
 		this.matrix = matrix;
 	}
+	public void setPoint(double x , double y , char c) {
+	if	(x < 0 || y < 0 || y >= matrix.length || x >= matrix[0].length ) return;
+	int x_rounded = round(x);
+	int y_rounded = round(y);
+	matrix[y_rounded][x_rounded] = c ;
+	}
+	public void drawMatrix(double x, double y, int[][] matrix ,char c) {
+		for(int i =0; i < matrix.length ; i++)
+			for(int j = 0 ; j < matrix[0].length ;j++ ) {
+				if(matrix[i][j] != 0)
+				setPoint(x + j , y + i, c);
+			}
+	}
+	private int round(double x) {
+		if(x - (int) x >= 0.5) return (int) x + 1 ;
+		return (int) x;
+	}
 
 }
